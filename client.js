5215b2f50f835ccf37a8aecca094bdf5b1154f25
Draor <jaucca@gmail.com>
2015-09-14 08:44:54 +0300

    client ja server-komentoja lisätty

--- client.js
+++ client.js
@@ -1,9 +1,34 @@
-
-
+var socket = io();
 
 function login(){
 	var user = document.getElementsByName("username");
 	var pswrd = document.getElementsByName("password");
+	//login button id
+	var loginButton = document.getElementsByName('tarttee nappulan nimen');
+
+
+
+
+
 
 	
 }
+
+
+function register(){
+	var regEmail = document.getElementsByName("email");
+	var regUser = document.getElementsByName("registername");
+	var regPswrd = document.getElementsByName("registerpassword");
+
+	//salasanan muodostus ja tietojen lähetys serverille
+
+	var hash = CryptoJS.SHA256(regPswr.value).toString();
+
+	//tietojen lähetys
+	socket.emit("registerUser", JSON.stringify({ email: regEmail.value, username: regUser.value, password: hash}));
+
+
+}
+
+//jo olevan käyttäjätiedon viesti
+socket.on("nameOnUse")
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
+// haetaan express
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
-
-
-
 
 // SQL-yhteys, yhteysparametrit default-asennossa
 var mysql = require('mysql');
@@ -18,6 +13,56 @@ var connection = mysql.createConnection({
 	password : '1234',
 	database : 'matodatabase'
 });
+
+
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connect', OnConnect);
+
+//Yhteys
+
+function OnConnect(socket){
+	console.log("Client has connected.");
+// kuuntelee rekisteröinti-eventtiä
+	socket.on("registerUser", registerUser);
+}
+
+
+function registerUser(userData)  {
+	var parsedData = JSON.parse(userData);
+	var email = parsedData["email"];
+	var username = parsedData["username"];
+	var hashpsw = parsedData["password"];
+
+	//DB-kysely
+
+	connection.query(
+		"SELECT kayttajatunnus FROM wormdb WHERE name = ?", [username],
+		function(err, row, db) {
+			if (row.length) {
+				//viestiä varatusta nimestä?
+				io.emit("nameOnUse", username);
+			}
+
+			else {
+				connection.query(
+					"INSERT INTO wormdb (email, kayttajatunnus, pswrd) VALUES (?. ?, ?)",
+					[email, username, hashpsw],
+					function(err, result){
+						if (result.affectedRows) {
+							io.emit("Tervetuloa käyttäjäksi ", username);
+						}
+					})
+			}
+		}
+}
+
 
 
 
